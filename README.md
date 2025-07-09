8a061f3b1ce6903edb78ca1b93c97d4495fd645f
Jorge11223344 <jimunozacuna+3@gmail.com>
2025-07-09 17:44:24 +0000

    W

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fasapi_postgres
+
+
+link para db: https://feanware.medium.com/dise%C3%B1o-de-una-api-crud-con-fastapi-en-python-y-sqlalchemy-749be700f897
--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from .database import SessionLocal, Base, engine
+from .crud import get_users
+from .models import User
+
+
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
+@app.get("/users")
 def read_root():
     return {"Hello": "World"}
 
